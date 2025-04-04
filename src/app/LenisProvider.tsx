5abb0f84c811cfd0e3cfdb1f3c2490f5f49643b7
05 Dewa Raditya Rochman <dewarochman26@gmail.com>
2025-04-04 22:43:32 +0700

    fix: ReactLenis dynamic import and LenisRef type for successful build

--- src/app/LenisProvider.tsx
+++ src/app/LenisProvider.tsx
@@ -1,10 +1,24 @@
 "use client";
 
-import { ReactLenis } from "@studio-freight/react-lenis";
+import dynamic from "next/dynamic";
+import React, { ReactNode } from "react";
 
-export default function LenisProvider({ children }: { children: React.ReactNode }) {
+const ReactLenis = dynamic(
+  () =>
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    import("@studio-freight/react-lenis").then((mod) => mod.ReactLenis as React.ComponentType<any>),
+  { ssr: false }
+);
+
+export default function LenisProvider({ children }: { children: ReactNode }) {
   return (
-    <ReactLenis root options={{ duration: 1.5, easing: (t) => Math.sqrt(1 - (t - 1) ** 2) }}>
+    <ReactLenis
+      root
+      options={{
+        duration: 1.5,
+        easing: (t: number) => Math.sqrt(1 - (t - 1) ** 2),
+      }}
+    >
       {children}
     </ReactLenis>
   );
